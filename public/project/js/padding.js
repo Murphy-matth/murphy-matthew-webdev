485223c377bb24eaddf3b0e5acc6f5366d966e37
Matthew <murphy1022@gmail.com>
2017-05-19 09:17:22 -0400

    Add responsive iframe

--- public/project/js/padding.js
+++ public/project/js/padding.js
@@ -5,14 +5,14 @@
 (function() {
     var onResize = function() {
         // apply dynamic padding at the top of the body according to the fixed navbar height
-        $("body").css("padding-top", $(".navbar-fixed-top").height());
+        jQuery("body").css("padding-top", jQuery(".navbar-fixed-top").height());
     };
 
     // attach the function to the window resize event
-    $(window).resize(onResize);
+    jQuery(window).resize(onResize);
 
     // call it also when the page is ready after load or reload
-    $(function() {
+    jQuery(function() {
         onResize();
     });
 })();
